fbd73cec15afeadb2d5987b77d7cef7617e778c5
Dom <dominicd7@hotmail.com>
2024-05-13 22:57:04 +0200

    delete build

--- build/addon/prefs.js
+++
@@ -1,4 +0,0 @@
-/* eslint-disable no-undef */
-// pref("extensions.cita.show-icons", true);
-// pref("extensions.cita.enable-keyboard-shortcuts", true);
-// pref("extensions.cita.label-new-items", false);
