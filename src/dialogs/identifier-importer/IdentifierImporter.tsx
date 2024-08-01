c353cb348f9957cc95934190cb8176fb792a0459
Dominic D <dominicd7@hotmail.com>
2024-08-01 13:55:13 +0000

    Fix identifier importer (#277)

--- src/dialogs/identifier-importer/IdentifierImporter.tsx
+++ src/dialogs/identifier-importer/IdentifierImporter.tsx
@@ -1,22 +1,28 @@
-import PropTypes from "prop-types";
-import React from "react";
+import * as React from "react";
+import * as PropTypes from "prop-types";
 
-const IdentifierImporter = (props: any) => (
+const IdentifierImporter = (props: {
+	getString: (name: string) => string;
+	onCancel: () => void;
+	onImport: () => void;
+}) => (
 	// TS gives an error about using orient here
 	// <div orient="vertical">
 	<div>
 		<div id="identifier-importer-description">
-			<label>{props.getString("identifier-importer.description")}</label>
+			<label>
+				{props.getString("wikicite.identifier-importer.description")}
+			</label>
 		</div>
 		<div id="identifier-importer-textbox">
 			<textarea id="identifier-input" rows={5} autoFocus />
 		</div>
 		<div id="identifier-importer-buttons">
 			<button onClick={props.onCancel}>
-				{props.getString("identifier-importer.cancel")}
+				{props.getString("wikicite.identifier-importer.cancel")}
 			</button>
 			<button onClick={props.onImport}>
-				{props.getString("identifier-importer.add")}
+				{props.getString("wikicite.identifier-importer.add")}
 			</button>
 		</div>
 	</div>
