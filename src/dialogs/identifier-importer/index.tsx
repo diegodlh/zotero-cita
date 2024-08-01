5142548bd71010fb9bd4ffb77bc6ea4724a0b6c8
Dom <dominicd7@hotmail.com>
2024-08-01 15:54:00 +0200

    Fix identifier importer

--- src/dialogs/identifier-importer/index.tsx
+++ src/dialogs/identifier-importer/index.tsx
@@ -1,14 +1,9 @@
 import IdentifierImporter from "./IdentifierImporter";
-import React from "react";
-import ReactDOM from "react-dom";
+import * as React from "react";
+import { createRoot } from "react-dom/client";
 
-declare const Components: any;
-
-// import Services into the new window
-Components.utils.import("resource://gre/modules/Services.jsm");
-
-const { Wikicite } = window.arguments[0];
-const retVals = window.arguments[1];
+const { Wikicite } = (window as any).arguments[0];
+const retVals: { text?: string } = (window as any).arguments[1];
 
 function onCancel() {
 	window.close();
@@ -23,12 +18,12 @@ function onImport() {
 
 window.addEventListener("load", () => {
 	document.title = Wikicite.getString("wikicite.identifier-importer.title");
-	ReactDOM.render(
+	const root = createRoot(document.getElementById("root")!);
+	root.render(
 		<IdentifierImporter
 			getString={(name) => Wikicite.getString(name)}
 			onCancel={onCancel}
 			onImport={onImport}
 		/>,
-		document.getElementById("root"),
 	);
 });
