f900bfe407b87d99ee7189471013b13bd555e904
Dom <dominicd7@hotmail.com>
2024-08-07 20:05:33 +0200

    Add custom UI for selecting from partial QID matches

---
+++ src/dialogs/selector/Selector.tsx
@@ -0,0 +1,39 @@
+import * as React from "react";
+import * as PropTypes from "prop-types";
+
+const Selector = (props: {
+	choices: string[];
+	message: string;
+	getString: (name: string) => string;
+	onCancel: () => void;
+	onConfirm: () => void;
+}) => (
+	<div>
+		<div id="selector-description">
+			<label>{props.message}</label>
+		</div>
+		<select size={10} id="selector-list">
+			{props.choices.map((text) => (
+				<option>{text}</option>
+			))}
+		</select>
+		<div id="selector-buttons">
+			<button onClick={props.onCancel}>
+				{props.getString("wikicite.wikidata.reconcile.approx.cancel")}
+			</button>
+			<button onClick={props.onConfirm}>
+				{props.getString("wikicite.wikidata.reconcile.approx.confirm")}
+			</button>
+		</div>
+	</div>
+);
+
+Selector.propTypes = {
+	choices: PropTypes.arrayOf(PropTypes.string),
+	message: PropTypes.string,
+	getString: PropTypes.func,
+	onCancel: PropTypes.func,
+	onConfirm: PropTypes.func,
+};
+
+export default Selector;
